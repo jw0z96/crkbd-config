53322e2d5b838ead623e6ca57e036d1a77beb507
jw0z96 <25467950+jw0z96@users.noreply.github.com>
2023-04-12 22:35:45 +0100

    revert vial

--- keymap.c
+++ keymap.c
@@ -27,8 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-/*                                        TL_UPPR, KC_LGUI,  KC_SPC,     KC_ENT, TL_LOWR, KC_RALT */ // Requires newer QMK
-                                          FN_MO23, KC_LGUI,  KC_SPC,     KC_ENT, FN_MO13, KC_RALT
+                                          TL_UPPR, KC_LGUI,  KC_SPC,     KC_ENT, TL_LOWR, KC_RALT
                                       //`--------------------------'  `--------------------------'
 
   ),
--- rules.mk
+++ rules.mk
@@ -1,8 +1,6 @@
 MOUSEKEY_ENABLE = no     # Mouse keys
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 VIA_ENABLE      = yes    # Enable VIA
-VIAL_ENABLE     = yes    # Enable VIAL
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
-VIAL_INSECURE   = yes    # ?
